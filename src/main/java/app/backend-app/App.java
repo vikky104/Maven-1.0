bfdcd9333037eaae916b546677ac456c8b8bbd64
tejeshwar <tejeshwar104@gmail.com>
2024-02-29 11:34:15 +0000

    maven project template

---
+++ src/main/java/app/backend-app/App.java
@@ -0,0 +1,13 @@
+package app.backend-app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
